d2fa0c25ce42e9ea1edc9656de5907b59cdfbd25
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-07-17 16:57:38 +0200

    deleted commit class, updated branchDto class

--- src/main/java/com/githubscanner/nonfork_finder/dto/BranchDto.java
+++ src/main/java/com/githubscanner/nonfork_finder/dto/BranchDto.java
@@ -1,9 +1,29 @@
 package com.githubscanner.nonfork_finder.dto;
 
-import com.githubscanner.nonfork_finder.model.Commit;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
-public record BranchDto(String name, Commit commit) {
-    public String lastCommitSha() {
-        return commit != null ? commit.getSha() : null;
+@Getter
+@Setter
+public class BranchDto {
+    private String name;
+    private Map<String, Object> commit;
+
+    @JsonProperty("commit")
+    public void setCommit(Map<String, Object> commit) {
+        this.commit = commit;
+    }
+
+    @JsonIgnore
+    public Map<String, Object> getCommit() {
+        return commit;
+    }
+
+    @JsonProperty("lastCommitSha")
+    public String getLastCommitSha() {
+        return commit != null ? (String) commit.get("sha") : null;
     }
 }

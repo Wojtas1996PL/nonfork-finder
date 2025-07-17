8832b3a72c3f0d09ee2dfbb696a69edd0dd02023
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-07-17 16:54:37 +0200

    updated exceptions

--- src/main/java/com/githubscanner/nonfork_finder/exception/EmptyGitHubResponseException.java
+++ src/main/java/com/githubscanner/nonfork_finder/exception/EmptyGitHubResponseException.java
@@ -1,7 +1,13 @@
 package com.githubscanner.nonfork_finder.exception;
 
+import lombok.Getter;
+
+@Getter
 public class EmptyGitHubResponseException extends RuntimeException {
-    public EmptyGitHubResponseException(String message) {
+    private final int status;
+
+    public EmptyGitHubResponseException(int status, String message) {
         super(message);
+        this.status = status;
     }
 }
--- src/main/java/com/githubscanner/nonfork_finder/exception/UserNotFoundException.java
+++ src/main/java/com/githubscanner/nonfork_finder/exception/UserNotFoundException.java
@@ -1,7 +1,13 @@
 package com.githubscanner.nonfork_finder.exception;
 
+import lombok.Getter;
+
+@Getter
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
+    private final int status;
+
+    public UserNotFoundException(int status, String message) {
         super(message);
+        this.status = status;
     }
 }
